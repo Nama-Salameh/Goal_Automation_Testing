e9b446f7688f39bd262bd3588fe6fb5231db3c3e
RaghadKhatatba <RaghadZKhatatba@gmail.com>
2023-05-15 18:35:52 +0300

    Added comments to the code

--- cypress/e2e/SearchForStaffFeatcher.cy.js
+++ cypress/e2e/SearchForStaffFeatcher.cy.js
@@ -7,25 +7,32 @@ beforeEach(() =>{
   cy.wait(2000);
   cy.visit('https://goal-dev.mdx.ac.uk/staff/30/staffs/');
   })
-
+ //check open staff page
    it('Check Open Staff page', () => {
     cy.get('table.table').should('exist');
    });
-
+ //Check Staff page contain Input for Search
    it('Check Staff page contain Input for Search', () => {
     cy.wait(2000);
     cy.get('input[type="search"]').should('exist')
    });
-   
+   //Check Search for staff that exsits in the staff list .
    it('Check Search for staff that exsits in the staff list .', () => {
+     //write RaghadKh in input search
         cy.get('input[type="search"]').type('RaghadKh');
         cy.wait(2000);
+        //chick that tabel contain RaghadKh
         cy.get('table').should('contain', 'RaghadKh')
+        //chick that table contain one recored
        cy.get('table').should('have.length', 2);
       });
+      //Check Search for staff that not exsits in the staff list .
       it('Check Search for staff that not exsits in the staff list .', () => {
+        //write yy in input search
         cy.get('input[type="search"]').type('yy');
         cy.wait(2000);
+        //chick that tabel not contain yy
         cy.get('table').should('not.contain', 'yy')
+        //chick that table only contain 'No matching records found'
         cy.get('table').should('contain', 'No matching records found')
       });
--- cypress/e2e/deleteStaff.cy.js
+++ cypress/e2e/deleteStaff.cy.js
@@ -7,22 +7,29 @@ beforeEach(() =>{
   cy.wait(2000);
   cy.visit('https://goal-dev.mdx.ac.uk/staff/30/staffs/');
   })
-
+ //check open staff page
    it('Check Open Staff page', () => {
     cy.get('table.table').should('exist');
    });
+   //Check Remove exsisting staff 
    it('Check Remove exsisting staff ', () => {
+    //click on Raghad
    cy.get('tr#staff_Raghad').click();
    cy.wait(2000);
-  cy.get('a[data-href="Raghad"]').click({ force: true });
+   //click on remove
+   cy.get('a[data-href="Raghad"]').click({ force: true });
     cy.wait(2000);
+    //click remove on aleart
     cy.get('a.btn.btn-danger.btn-ok[data-href="Raghad"]').click();
    });
    it('Check cancel Remove exsisting staff ', () => {
+     //click on RaghadKh
     cy.get('tr#staff_RaghadKh').click();
     cy.wait(2000);
+     //click on remove
     cy.get('#staff_RaghadKh a[data-href="RaghadKh"]').click({ force: true });
     cy.wait(2000);
+    //click cancel remove in aleart
     cy.get('.modal-footer').contains('Cancel').click();
    });
 
